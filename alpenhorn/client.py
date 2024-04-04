ce168303735422ce4602b9d4977aedbefbdaf68b
Liam Gray <lgray@phas.ubc.ca>
2024-04-04 16:17:30 -0700

    style(black): updated black formatting

--- alpenhorn/client.py
+++ alpenhorn/client.py
@@ -1,4 +1,5 @@
 """Alpenhorn client interface."""
+
 # === Start Python 2/3 compatibility
 from __future__ import absolute_import, division, print_function
 from future.builtins import *  # noqa  pylint: disable=W0401, W0614
